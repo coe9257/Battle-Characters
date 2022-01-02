f1d5e871f6f7fa1c2f96c6470754f1ce94d8f6e0
William <coe9257@gmail.com>
2022-01-01 21:59:48 -0800

    All done. I need to implement miss chance now

--- main.js
+++ main.js
@@ -21,6 +21,12 @@ function initialPlayerMoveLoad() {
 }
 initialPlayerMoveLoad();
 
+function startOver() {
+    // document.querySelector('.space-bottom').style.backgroundColor = "green";
+    // document.querySelector('.space-bottom').style.color = "blue";
+    // document.querySelector('.space-bottom').textContent = "Play Again?";
+}
+
 class Player { 
     constructor(name, life, armor, strength) {
         this.name = name;
@@ -35,16 +41,27 @@ class Player {
 
     defend(strength) {
         this.life -= strength;
+        let player = this.name
         let playerHTMLScore = '.' + this.name + `-score`;
+        let playerTurn = '.' + player + '-turn';
         let life = this.life;
 
-        function updateLife() {
-            document.querySelector(playerHTMLScore).textContent = life;
+        function lifeCheck() {
+            if (life <= 0) {
+                document.querySelector(`.` + player).textContent = "DEAD";
+                    console.log(playerTurn);
+                document.querySelector(`.` + player + '-turn').style.backgroundColor = "red";
+                document.querySelector(`.` + player + '-score').textContent = 0;
+                startOver();
+            } else {
+                function updateLife() {
+                    document.querySelector(playerHTMLScore).textContent = life;
+                }
+                updateLife();
+            }
         }
-        updateLife();
-
+        lifeCheck()
     }
-
 }
 
 let player1 = new Player("player1", 55, 25, 18);
@@ -52,24 +69,29 @@ let player2 = new Player("player2", 70, 15, 26);
 
 document.querySelector(".player1-turn").addEventListener("click", function() {
     if (document.querySelector(".player1-turn").style.backgroundColor == "green") {
-        player1.attack(player2);
         document.querySelector('.player1-turn').style.backgroundColor = "red";
         document.querySelector('.player2-turn').style.backgroundColor = "green";
         document.querySelector('.player1-turn').textContent = "STOP";
         document.querySelector('.player2-turn').textContent = "GO";
+        player1.attack(player2);
     };
 });
 
 document.querySelector(".player2-turn").addEventListener("click", function() {
     if (document.querySelector(".player2-turn").style.backgroundColor == "green") {
-        player2.attack(player1)
         document.querySelector('.player1-turn').style.backgroundColor = "green";
         document.querySelector('.player2-turn').style.backgroundColor = "red";
         document.querySelector('.player1-turn').textContent = "GO";
         document.querySelector('.player2-turn').textContent = "STOP";
+        player2.attack(player1)
     }
 });
 
+document.querySelector(".player2-turn").addEventListener("click", function() {
+    if (document.querySelector(".space-bottom").style.backgroundColor == "green") {
+
+    }
+});
 
 
 
