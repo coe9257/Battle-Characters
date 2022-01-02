41b80e4ae3949dbf99817c6ede4e585deefb88be
William <coe9257@gmail.com>
2022-01-01 23:27:51 -0800

    Damage Fixed - Dodge updated; Dead Fixed

--- main.js
+++ main.js
@@ -27,12 +27,30 @@ function startOver() {
     // document.querySelector('.space-bottom').textContent = "Play Again?";
 }
 
+function dodged(strength, dodge) {
+    let mathRdm = Math.floor(Math.random() * 101);
+    let dodgeChance = dodge - (strength / 2);
+        // console.log(`mathRDM: `, mathRdm);
+        // console.log(`dodgeChance: `, dodgeChance);
+
+    let chance = false;
+
+    if (dodgeChance > mathRdm) {
+        chance = true;
+        console.log("DODGED!");
+        document.querySelector('.footer').textContent = "";
+    }
+    return chance;
+
+}
+
 class Player { 
-    constructor(name, life, armor, strength) {
+    constructor(name, life, armor, strength, agility) {
         this.name = name;
         this.life = life;
         this.armor = armor;
         this.strength = strength;
+        this.agility = agility;
     }
 
     attack(opponent) {
@@ -40,32 +58,63 @@ class Player {
     }
 
     defend(strength) {
-        this.life -= strength;
+
+        let dChance = dodged(strength, this.agility)
+
         let player = this.name
         let playerHTMLScore = '.' + this.name + `-score`;
         let playerTurn = '.' + player + '-turn';
-        let life = this.life;
+        let armor = this.armor;
+
+        console.log(`dChance: ${dChance}`);
+
+        if (dChance == true) {
+            document.querySelector(".footer").textContent = `${player} has dodged!`;
+        }
+
+        if (dChance != true) {
+            console.log(`armor: ${armor}; strength: ${strength}`)
+            console.log(`calculation`, ((100 - armor) / 100) * strength );
+            let takenDamage = (((100 - armor) / 100) * strength);
+            document.querySelector('.footer').textContent = `${player} 
+                life (${Math.trunc(this.life)}) takes ${Math.trunc(takenDamage)} damage`;
+                console.log(`takenDamage: ${takenDamage}; Life: ${this.life}`);
+
+                console.log(`life before: ${this.life}`);
+            this.life -= takenDamage;
+                console.log(`life after: ${this.life}`);
+        }
+
+        let currentLife = this.life;
+
+        function updateLife() {
+            document.querySelector(playerHTMLScore).textContent = `${Math.trunc(currentLife)}`;
+        }
+        updateLife();
+
+        dChance = false;
 
         function lifeCheck() {
-            if (life <= 0) {
+            if (currentLife < 1) {
                 document.querySelector(`.` + player).textContent = "DEAD";
                     console.log(playerTurn);
                 document.querySelector(`.` + player + '-turn').style.backgroundColor = "red";
                 document.querySelector(`.` + player + '-score').textContent = 0;
                 startOver();
-            } else {
-                function updateLife() {
-                    document.querySelector(playerHTMLScore).textContent = life;
-                }
-                updateLife();
             }
         }
         lifeCheck()
     }
 }
 
-let player1 = new Player("player1", 55, 25, 18);
-let player2 = new Player("player2", 70, 15, 26);
+let player1 = new Player("player1", 55, 75, 18, 95);
+let player2 = new Player("player2", 70, 65, 26, 5);
+
+function loadLife() {
+    document.querySelector('.player1-score').textContent = player1.life;
+    document.querySelector('.player2-score').textContent = player2.life;
+}
+loadLife();
 
 document.querySelector(".player1-turn").addEventListener("click", function() {
     if (document.querySelector(".player1-turn").style.backgroundColor == "green") {
